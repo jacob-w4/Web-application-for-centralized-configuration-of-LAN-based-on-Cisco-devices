59cda6bfb36e6a5e4384a01d59d9cffead2822c3
Jakub <jakubplewa00@gmail.com>
2025-06-09 16:16:32 +0200

    Create Live and Develoment config file

---
+++ config.py
@@ -0,0 +1,16 @@
+class Development():
+    # Flask Config
+    DEBUG = True
+    FLASK_PORT = 2888
+    HOST = '127.0.0.1'
+
+    # Database Config
+
+
+class Live():
+    # Flask Config
+    DEBUG = False
+    FLASK_PORT = 2888
+    HOST = '0.0.0.0'
+
+    # Database Config
