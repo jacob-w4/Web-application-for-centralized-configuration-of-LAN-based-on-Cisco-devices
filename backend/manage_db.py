5d04fb140d0761b4925e5938633e2381b6a548a0
Jakub <jakubplewa00@gmail.com>
2025-06-14 23:00:16 +0200

    Add manage_db file

---
+++ backend/manage_db.py
@@ -0,0 +1,19 @@
+from app import create_app, db
+from config import Config
+from app.models import *
+
+
+
+app = create_app(Config)
+
+# Create database structure
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    # Create admin user
+    admin = User(username=app.config['AD_USER'],
+                 password=app.config['AD_PASSW'],
+                 permission='admin')
+    db.session.add(admin)
+    db.session.commit()
