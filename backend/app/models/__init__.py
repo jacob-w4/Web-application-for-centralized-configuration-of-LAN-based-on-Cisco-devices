685838cb0b19db1ae88e00b8d8d8af518ce07081
Jakub <jakubplewa00@gmail.com>
2025-06-14 22:57:51 +0200

    Add init file

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .device import Device
+from .device_type import DeviceType
+from .backup import Backup
+from .device_folder import DeviceFolder
+from .user_device import UserDevice
