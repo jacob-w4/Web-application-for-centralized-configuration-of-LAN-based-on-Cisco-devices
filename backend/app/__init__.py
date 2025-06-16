7f5d8fb6abf8931c658efdf4be3798937f76dabe
Jakub <jakubplewa00@gmail.com>
2025-06-16 14:18:22 +0200

    Add flask_jwt_extended

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app(config_class):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+    jwt.init_app(app)
 
     # Blueprints
     from .views import views
