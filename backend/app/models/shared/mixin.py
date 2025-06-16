d3744e56970483e74187c1d15386c12d95348f4e
Jakub <jakubplewa00@gmail.com>
2025-06-16 14:15:43 +0200

    Create BaseMixin

---
+++ backend/app/models/shared/mixin.py
@@ -0,0 +1,10 @@
+from app import db
+
+class BaseMixin():
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
