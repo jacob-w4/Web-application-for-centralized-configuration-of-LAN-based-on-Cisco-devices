3b8355003daae1613714af2ffc58ced342cff2d0
Jakub <jakubplewa00@gmail.com>
2025-06-19 14:26:09 +0200

    Add flask-cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,21 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
 jwt = JWTManager()
+cors = CORS(supports_credentials=True)
 
 def create_app(config_class):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    cors.init_app(app)
     db.init_app(app)
     jwt.init_app(app)
+    
 
     # Blueprints
     from .views import views
     app.register_blueprint(views, url_prefix='/api')
+    
 
     return app
     
