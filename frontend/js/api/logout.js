f085b13b20d2e781675fec430da6a7049f1d7bc9
Jakub <jakubplewa00@gmail.com>
2025-06-26 13:46:30 +0200

    Create new file structure

---
+++ frontend/js/api/logout.js
@@ -0,0 +1,22 @@
+async function logout() {
+  try {
+    const response = await fetch(`${window.CONFIG.API_URL}/logout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    });
+    if (response.ok) {
+      window.location.href = 'index.html'
+    } else {
+      console.error('Błąd logowania:', error);
+      alert('Wystąpił błąd. Spróbuj ponownie później.');
+    }
+  } catch (error) {
+    console.error('Błąd logowania:', error);
+    alert('Wystąpił błąd. Spróbuj ponownie później.');
+  }
+}
+
+document.getElementById('logout-btn').addEventListener('click', logout);
---
+++ frontend/js/api/session.js
@@ -0,0 +1,31 @@
+async function check_session() {
+  try {
+    const response = await fetch(`${window.CONFIG.API_URL}/me`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    });
+
+    const result = await response.json()
+
+    if (!response.ok) {
+        alert('Token is missing')
+        window.location.href = 'index.html'
+    } else {
+      if (result.perm == 'admin') {
+        const users_div = document.getElementById('users')
+        users_div.classList.replace('hidden', 'flex')
+      }
+    }
+  } catch (error) {
+    console.error('Błąd logowania:', error);
+    alert('Wystąpił błąd. Spróbuj ponownie później.');
+    //window.location.href = 'index.html'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    check_session();
+});
