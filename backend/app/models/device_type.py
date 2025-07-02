8a918df6eb895e5dedbcbf4af4f91fbb71b3ed8c
Jakub <jakubplewa00@gmail.com>
2025-07-02 18:52:32 +0200

    Add new method find_by_name_and_vendor

--- backend/app/models/device_type.py
+++ backend/app/models/device_type.py
@@ -8,7 +8,13 @@ class DeviceType(db.Model, BaseMixin):
     vendor = db.Column(db.String(45), nullable=False)
     devices = db.relationship('Device', backref='device_type')
 
-    
+    def __init__(self, name, vendor):
+        self.name = name
+        self.vendor = vendor
+
+    @classmethod
+    def find_by_name_and_vendor(cls, name, vendor):
+        return cls.query.filter_by(name=name, vendor=vendor).first()    
 
     def __repr__(self):
         return '<DeviceType %r>' % self.name
