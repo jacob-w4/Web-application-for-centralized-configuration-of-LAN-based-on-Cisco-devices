d4a6a209f451995467fcd2f1d924b0d5801a7592
Jakub <jakubplewa00@gmail.com>
2025-07-02 18:49:08 +0200

    Test fixed

--- backend/tests/unit/test_models.py
+++ backend/tests/unit/test_models.py
@@ -20,10 +20,9 @@ def test_new_device():
     WHEN a new device is created
     THEN check name, ipv4_address, ipv6_address, description and model are defined correctly
     """
-    device = Device('test_device', '192.168.1.100', '2001:db8:acab:cafe::1', 'desc123', 'c1921/k9')
+    device = Device(name='test_device', ipv4_address='192.168.1.100', description='desc123', model='c1921/k9')
 
     assert device.name == 'test_device'
     assert device.ipv4_address == '192.168.1.100'
-    assert device.ipv6_address == '2001:db8:acab:cafe::1'
     assert device.description == 'desc123'
     assert device.model == 'c1921/k9'
