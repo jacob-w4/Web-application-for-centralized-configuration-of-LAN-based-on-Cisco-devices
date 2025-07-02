4f5c765563b3c88eca7d058938a700ce1720892f
Jakub <jakubplewa00@gmail.com>
2025-07-02 18:49:39 +0200

    Add new client

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,6 +5,7 @@ from app import create_app
 from flask_jwt_extended import create_access_token, set_access_cookies
 import os
 from dotenv import find_dotenv, load_dotenv
+from app.models import User
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -22,13 +23,32 @@ def client(app):
 
 
 @pytest.fixture()
-def logged_in_client(client):
+def logged_in_admin_client(client):
     response = jsonify({'msg': 'Logged In'})
     access_token = create_access_token(identity=os.getenv('ADMIN_USERNAME'))
     set_access_cookies(response, access_token)
 
     client.set_cookie(
+        path='/',
         key='access_token_cookie',
-        value=access_token
+        value=access_token,
     )
     return client
+
+@pytest.fixture()
+def logged_in_client(client):
+    # Creating test user and save to db
+    user = User('testing123', 'test123', 'read')
+    user.save()
+
+    response = jsonify({'msg': 'Logged In'})
+    access_token = create_access_token(identity='testing123')
+    set_access_cookies(response, access_token)
+
+    client.set_cookie(
+        path='/',
+        key='access_token_cookie',
+        value=access_token,
+    )
+    yield client
+    user.delete()
